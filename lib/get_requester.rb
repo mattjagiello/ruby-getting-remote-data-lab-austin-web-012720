04971c5b7d31b8213b7e6fe505fa8474f1b81490
Matthew Jagiello <mjagiello@gmail.com>
2020-02-10 06:00:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,11 +10,11 @@ class GetRequester
   def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
-    response.body
+    @response.body
   end
 
   def parse_json
-    JSON.parse(response.body)
+    JSON.parse(@response.body)
   end
 
 end
